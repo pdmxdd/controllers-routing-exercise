a6adfc1ac0acd3385248f387f7a478045aff5f32
pdmxdd <pdmxdd@gmail.com>
2021-07-12 09:32:33 -0500

    chapter 10 exercise starter

---
+++ src/main/java/org/launchcode/controllersroutingexercise/ControllersRoutingExerciseApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.controllersroutingexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllersRoutingExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllersRoutingExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/controllersroutingexercise/ControllersRoutingExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.controllersroutingexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllersRoutingExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
